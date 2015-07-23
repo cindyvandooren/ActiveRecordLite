a659be57622f8e713c565674f36b5161471e8eae
Cindy Van Dooren <cindy.vandooren@gmail.com>
2015-07-22 18:00:01 -0700

    Finished project

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -4,11 +4,8 @@ require 'byebug'
 
 # Phase IIIa
 class AssocOptions
-  attr_accessor(
-    :foreign_key,
-    :class_name,
-    :primary_key
-  )
+  attr_accessor :foreign_key, :class_name, :primary_key
+
 
   def model_class
     @class_name.constantize
@@ -21,48 +18,68 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # debugger
-    @foreign_key = options[:foreign_key].nil? ? "#{name}_id".to_sym : options[:foreign_key]
-    @primary_key = options[:primary_key].nil? ? :id : options[:primary_key]
-    @class_name = options[:class_name].nil? ? "#{name}".camelcase : options[:class_name]
+    defaults = {
+      :foreign_key => "#{name}_id".to_sym,
+      :class_name => name.to_s.camelcase,
+      :primary_key => :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    @foreign_key = options[:foreign_key].nil? ? "#{self_class_name.downcase}_id".to_sym : options[:foreign_key]
-    @primary_key = options[:primary_key].nil? ? :id : options[:primary_key]
-    @class_name = options[:class_name].nil? ? "#{name}".camelcase.singularize : options[:class_name]
+    defaults = {
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym,
+      :class_name => name.to_s.singularize.camelcase,
+      :primary_key => :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    options = BelongsToOptions.new(name, options)
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
 
     define_method(name) do
-      fk = self.send(options.foreign_key)
-      tmc = options.model_class
-      result = tmc.where(options.primary_key => fk).first
+      options = self.class.assoc_options[name]
+
+      # We use send here, because that way we can access the instance
+      # variables of the BelongsToOptions instance.
+      key_val = self.send(options.foreign_key)
+        options
+          .model_class
+          .where(options.primary_key => key_val)
+          .first
     end
   end
 
 
   def has_many(name, options = {})
-    # options = HasManyOptions.new(name, self.class, options)
+    self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
 
-    # define_method(name) do
-    #   fk = self.send(options.foreign_key)
-    #   tmc = options.model_class
-    #   result = tmc.where(options.primary_key => fk).first
-    #
-    #
-    # end
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.primary_key)
+      options
+        .model_class
+        .where(options.foreign_key => key_val)
+    end
   end
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -5,6 +5,33 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through_table = through_options.table_name
+      through_prim_key = through_options.primary_key
+      through_for_key = through_options.foreign_key
+
+      source_table = source_options.table_name
+      source_prim_key = source_options.primary_key
+      source_for_key = source_options.foreign_key
+
+      key_val = self.send(through_for_key)
+      results = DBConnection.execute(<<-SQL, key_val)
+        SELECT
+          #{source_table}.*
+        FROM
+          #{through_table}
+        JOIN
+          #{source_table}
+          ON
+          #{through_table}.#{source_for_key} = #{source_table}.#{source_prim_key}
+        WHERE
+          #{through_table}.#{through_prim_key} = ?
+      SQL
+
+      source_options.model_class.parse_all(results).first
+    end
   end
 end
